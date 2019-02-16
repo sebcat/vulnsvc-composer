2ac4b39cfe27542af30942c30976ec7c9c51254f
Sebastian Cato <seb.cato@gmail.com>
2019-02-16 10:31:55 +0100

    Move comment before call to respond

--- src/index.php
+++ src/index.php
@@ -44,8 +44,8 @@ foreach ($jsondata->packages as $package) {
     $res = $matcher->match($package->name, $package->version);
     $vulns = array_merge($vulns, $res);
   } catch (Exception $e) {
-    respond(FALSE, "match failure at package index $i");
     /* TODO: log exception? */
+    respond(FALSE, "match failure at package index $i");
   }
   $i++;
 }
